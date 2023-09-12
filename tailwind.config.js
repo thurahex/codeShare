462c575714ffb77be1fae6c227363e6d5914513f
thurahex <thura.workmail@gmail.com>
2023-09-12 14:58:26 +0630

    color correct

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js,css}"],
   theme: {
+    colors : {
+      "primary" : "#0D41E1",
+      "secondary" : "#a8dadc",
+      "back-up" : "#1d3557",
+      "white":"#ffff",
+      "danger" : "#e63946",
+
+    },
+    fontFamily:{
+      'Rubik':['Rubik, sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
